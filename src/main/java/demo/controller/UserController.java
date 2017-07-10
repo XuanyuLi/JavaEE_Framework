92467bed05ee6f182f3b59285f3c3c8742c1b162
Administrator <2758697752@qq.com>
2017-07-10 17:33:27 +0800

    update

---
+++ src/main/java/demo/controller/UserController.java
@@ -0,0 +1,23 @@
+package demo.controller;
+
+import demo.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by lixuanyu
+ * on 2017/7/10.
+ */
+@Controller
+@RequestMapping("user")
+
+public class UserController {
+
+    @RequestMapping("create")
+    private String create(User user) {
+        // TODO: 7/10/17 save user...
+        System.out.println("create...");
+        return "redirect:/index.jsp";
+    }
+}
+
